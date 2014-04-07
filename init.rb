a14d1042432e84c82c55370647b041c13969c082
Babar Rehman <Babar.rehman@arkhitech.com>
2014-04-07 16:41:48 +0500

    First commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_mentions do
+  name 'Redmine Mentions plugin'
+  author 'Arkhitech'
+  description 'This is a plugin for Redmine which gives suggestions on using username in comments'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'https://github.com/arkhitech/redmine_mentions'
+end
