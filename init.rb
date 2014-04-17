c986ceca276b1b17cfa6f3ac3d488c2a15f90fe7
Babar Rehman <Babar.rehman@arkhitech.com>
2014-04-17 10:55:21 +0500

    Instead of over riding the view, a hook is used

--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 require 'redmine'
+
 Rails.configuration.to_prepare do
-   require_dependency 'journal'
+  require_dependency 'redmine_mentions/hooks'
+  require_dependency 'journal'
   Journal.send(:include, RedmineMentions::JournalPatch)
 end
 Redmine::Plugin.register :redmine_mentions do
@@ -8,7 +10,7 @@ Redmine::Plugin.register :redmine_mentions do
   author 'Arkhitech'
   description 'This is a plugin for Redmine which gives suggestions on using username in comments'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'https://github.com/arkhitech/redmine_mentions'
+  url 'https://github.com/arkhitech/redmine_mentions'
+  author_url 'http://www.arkhitech.com/'
   settings :default => {'trigger' => '@'}, :partial => 'settings/mention'
 end
---
+++ lib/redmine_mentions/hooks.rb
@@ -0,0 +1,13 @@
+module RedmineMentions
+  class Hooks < Redmine::Hook::ViewListener
+    # This just renders the partial in
+    # app/views/hooks/my_plugin/_view_issues_form_details_bottom.rhtml
+    # The contents of the context hash is made available as local variables to the partial.
+    #
+    # Additional context fields
+    #   :issue  => the issue this is edited
+    #   :f      => the form object to create additional fields
+    render_on :view_issues_edit_notes_bottom,
+              :partial => 'hooks/redmine_mentions/edit_mentionable'
+  end
+end
