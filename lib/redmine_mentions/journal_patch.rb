fb7fffe5d9d9fd2d45ff5228661bb7c627e98510
Steve Schwartz <steve@alfajango.com>
2015-01-05 11:28:47 -0500

    Updated view and journal_patch to force AR association to array for delete_if method to work.

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -8,7 +8,7 @@ module RedmineMentions
           if self.journalized.is_a?(Issue) && self.notes.present?
             issue = self.journalized
             project=self.journalized.project
-            users=project.users.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
+            users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
             users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
             regex_for_email = '\B('+users_regex+')'
             regex = Regexp.new(regex_for_email)
