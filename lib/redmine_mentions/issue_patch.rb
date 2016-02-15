c525210edac2c72f79b0a6b01d6a479ec067ef52
fredsdc <frederico.camara@serpro.gov.br>
2016-02-15 17:04:42 -0200

    Fixes the regex. It is causing a problem highlighting and addressing users that have usernames that are part of other user's usernames (like Frank and Franklin, John and Johnson).

--- lib/redmine_mentions/issue_patch.rb
+++ lib/redmine_mentions/issue_patch.rb
@@ -9,7 +9,7 @@ module RedmineMentions
           project=self.project
           users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
           users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
-          regex_for_email = '\B('+users_regex+')'
+          regex_for_email = '\B('+users_regex+')\b'
           regex = Regexp.new(regex_for_email)
           mentioned_users = self.description.scan(regex)
           usernames = []
--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -10,7 +10,7 @@ module RedmineMentions
             project=self.journalized.project
             users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
             users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
-            regex_for_email = '\B('+users_regex+')'
+            regex_for_email = '\B('+users_regex+')\b'
             regex = Regexp.new(regex_for_email)
             
             mentioned_users = self.notes.scan(regex)
