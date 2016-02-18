3eac3bd08bc887df8df67d7c037649d7bd3266b7
fredsdc <frederico.camara@serpro.gov.br>
2016-02-18 12:05:01 -0200

    Better layout for mail notification - inserts note as html

--- app/models/mention_mailer.rb
+++ app/models/mention_mailer.rb
@@ -1,4 +1,5 @@
 class MentionMailer < ActionMailer::Base
+  include ApplicationHelper
   layout 'mailer'
   default from: Setting.mail_from
   def self.default_url_options
@@ -9,6 +10,7 @@ class MentionMailer < ActionMailer::Base
   def notify_mentioning(issue, mentioned_by, mentioned_text, mentioned_user)
     @issue = issue
     @mentioned_by = mentioned_by
+    @mentioned_html = textilizable(mentioned_text)
     @mentioned_text = mentioned_text
     mail(to: mentioned_user.mail, subject: "[#{@issue.tracker.name} ##{@issue.id}] #{@mentioned_by} mentioned you in the issue: #{@issue.subject}")
   end
