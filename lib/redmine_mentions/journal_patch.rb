6a6dcebb933424c70c799e2ebf45976269158952
fredsdc <frederico.camara@serpro.gov.br>
2016-02-23 17:01:44 -0300

    Fix: notifies users only once

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -12,7 +12,7 @@ module RedmineMentions
             users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
             regex_for_email = '\B('+users_regex+')\b'
             regex = Regexp.new(regex_for_email)
-            mentioned_users = self.notes.scan(regex)
+            mentioned_users = self.notes.scan(regex).uniq
             mentioned_users.each do |mentioned_user|
               username = mentioned_user.first[1..-1]
               if user = User.find_by_login(username)
