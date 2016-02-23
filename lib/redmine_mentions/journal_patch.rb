1766e497d2438f14b4e56a1ed4e8911314117f7c
fredsdc <frederico.camara@serpro.gov.br>
2016-02-23 17:12:21 -0300

    Don't expose users login when notifying users. Wiki text becomes user name and link to user information.

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -9,13 +9,13 @@ module RedmineMentions
             issue = self.journalized
             project=self.journalized.project
             users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
-            users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
+            users_regex=users.collect{|u| "\"#{Regexp.escape("#{u.firstname} #{u.lastname}")}\":#{Setting.protocol}://#{Setting.host_name}#{Rails.application.routes.url_helpers.user_path(u)}".gsub(/(["\/])/, '\\\\\1')}.join('|')
             regex_for_email = '\B('+users_regex+')\b'
             regex = Regexp.new(regex_for_email)
             mentioned_users = self.notes.scan(regex).uniq
             mentioned_users.each do |mentioned_user|
-              username = mentioned_user.first[1..-1]
-              if user = User.find_by_login(username)
+              username = mentioned_user.first[1..-1].split("/").last
+              if user = User.find_by_id(username)
                 MentionMailer.notify_mentioning(issue, self, user).deliver
               end
             end
