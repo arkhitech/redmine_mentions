7cd5c60974276e986c09b3f91ad81701a07d18cd
Frederico Camara <frederico.camara@serpro.gov.br>
2016-03-22 19:41:28 -0300

    Fix: error when mentioning and linking issues

--- app/models/mention_mailer.rb
+++ app/models/mention_mailer.rb
@@ -1,6 +1,12 @@
+require 'roadie'
+
 class MentionMailer < ActionMailer::Base
-  include ApplicationHelper
   layout 'mailer'
+  helper :application
+
+  include Redmine::I18n
+  include Roadie::Rails::Automatic
+
   default from: Setting.mail_from
 
   helper :application
@@ -9,12 +15,10 @@ class MentionMailer < ActionMailer::Base
   def self.default_url_options
     Mailer.default_url_options
   end
-  
-  
+ 
   def notify_mentioning(issue, journal, user)
     @issue = issue
     @journal = journal
-    @htmlnote = textilizable(journal.notes)
     @textnote = ActionView::Base.full_sanitizer.sanitize(@htmlnote)
     mail(to: user.mail, subject: "[#{@issue.tracker.name} ##{@issue.id}] You were mentioned in: #{@issue.subject}")
   end
